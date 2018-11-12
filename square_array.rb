7f1ecf671c621c0b8f603be80c3a65fde3a6b37f
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 20:00:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   array.each do |number|
     square_number = number ** number
-    new_array << square_number
+    new_array.push(square_number)
   end 
   return new_array
 end

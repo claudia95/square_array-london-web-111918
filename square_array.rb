5bf94f28cd51391e6d494dceb1423c6218ee9317
Claudia Fox <claudiafox95@icloud.com>
2018-11-12 19:57:53 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   array.each do |number|
     square_number = number ** number
-    new_square_array << square_number
+    new_square_array.push(square_number)
   end  
 end
